2da8cce3e424b566535de44dac7756deca3ad386
Sandeep GC <sandeep_gc@hotmail.com>
2021-07-13 08:12:09 +1000

    Create a transaction on the BudgetStore db with readwrite access

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -65,3 +65,29 @@ function checkDatabase() {
     }
   };
 }
+
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
+
+  // Check if app is online before reading from db
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
+    checkDatabase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  // Create a transaction on the BudgetStore db with readwrite access
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  // Access your BudgetStore object store
+  const store = transaction.objectStore('BudgetStore');
+
+  // Add record to your store with add method.
+  store.add(record);
+};
+
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
